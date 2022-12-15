4c52860d9ed0d4c8aeb5e5475ba336c5694be707
indu kushwaha <goforindu@gmail.com>
2022-12-15 12:41:33 +0530

    first commit of use state hook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const[nationality, setNationality]=useState();
+  const[name, setName]=useState("");
+  const[counter, setCounter]=useState(0);
+  const[isVisible, setIsVisible]=useState(false);
+  const [names, setNames]=useState([]);
+  const showName=()=>{
+    setNationality("Indian");
+    console.log(name);
+  }
+  const increment=()=>{
+     setCounter(counter+1);
+  }
+  const decrement=()=>{
+    setCounter(counter-1);
+  }
+
+  const toggleText=()=>{
+    setIsVisible(!isVisible);
+  }
+  const addUser=(e)=>{
+    e.preventDefault();
+    setNames([...names,{id:names.length, name}]);
+   
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>I am, {nationality}</h1>
+      <button onClick={showName}>Click me</button>
+      <hr/>
+     
+      <button onClick={increment}>Increment</button>
+      <h1>{counter}</h1>
+      <button onClick={decrement}>Decrement</button>
+      <hr/>
+      {isVisible && <p>Now you can see me</p> }
+      <button onClick={toggleText}>ToggleDisplay</button>
+      <hr/>
+      <form onSubmit={addUser}>
+      <input type="text" placeholder="enter name" onChange={(e)=>{setName(e.target.value)}}/>
+      <button>AddUser</button>
+      </form>
+     <hr/>
+     {names.map((item)=>{
+     return <li key={item.id}>{item.name}</li>
+     })}
     </div>
+   
+
+   
   );
 }
 
